1144a68f5979963bf8704385f9aa799c86de7bb6
touhidpasha <touhidpasha552@gmail.com>
2021-09-07 17:56:53 +0530

    using constructor mood is analyzed

--- src/MyPackage/MoodAnalyzer.java
+++ src/MyPackage/MoodAnalyzer.java
@@ -1,14 +1,33 @@
 package MyPackage;
 
 public class MoodAnalyzer {
+	
+	MoodAnalyzer(String msg){ //parameterized constructor
+		if(msg.contains("SAD"))
+			System.out.println( "SAD");
+		else if(msg.contains("HAPPY"))
+			System.out.println( "HAPPY");
+		
+	}
+	
+	
+	public MoodAnalyzer() {
+		// TODO Auto-generated constructor stub
+	}
+
+
 	public String analyzeMood(String msg) {
 		if(msg.contains("SAD"))
 			return "SAD";
+		
+		else if(msg.contains("HAPPY"))
+			return( "HAPPY");
 		else 
-			return "HAPPY";
+			return null;
 	}
 	public static void main(String args[]) {
 		MoodAnalyzer m1=new MoodAnalyzer();
+		MoodAnalyzer m2=new MoodAnalyzer("be HAPPY ,don't worry");//calling parameterized construnctor
 		System.out.println(m1.analyzeMood("why u are SAD always..."));
 		
 	}
